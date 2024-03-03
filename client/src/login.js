baa552427a28b7611a1c6d860b4354dc2b09e37a
eeroverlord <112696364+eeroverlord@users.noreply.github.com>
2024-03-03 14:21:02 +0200

    Login UI testi 2

--- client/src/login.js
+++ client/src/login.js
@@ -1,4 +1,5 @@
-/* Kirjautumisen front-end funktio.
+/* Kirjautumisen front-end funktio
+Login komponentti, joka sisältää input-kentät käyttäjänimen ja salasanan syöttämiseen sekä login-napin.
 implementaatio => määrittele variablet username ja password,
 fetch() API:lla => /login endpoint palvelimen handleriin,
 */
