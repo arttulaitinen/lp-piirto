a7f7fe34884e2e9463a8ab01e23db35d5a607c0c
eeroverlord <112696364+eeroverlord@users.noreply.github.com>
2024-03-03 14:51:19 +0200

    App() ja components

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import DrawingArea from './components/DrawingArea';
 import Toolbar from './components/Toolbar';
-import "./App.css";
 import login from './components/login';
+import "./App.css";
 
 function App() {
+  // Login
+  const [user, setUser] = useState(null);
+  const [isLogin, setIsLogin] = useState(false);
+  const handleLogin = (user) => {
+    setUser(user);
+    setIsLogin(true);
+  }
   return (
     <div className="app-container">
       <Toolbar />
