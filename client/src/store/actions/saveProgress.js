3e7734bd9bed46bb1b7d91e37f7cc690225c50bf
eeroverlord <112696364+eeroverlord@users.noreply.github.com>
2024-03-19 16:46:22 +0200

    Korjaus

--- client/src/store/actions/saveProgress.js
+++ client/src/store/actions/saveProgress.js
@@ -1,22 +1,22 @@
-import {serializeState} from '../../utils/serializeState'
+import { serializeState } from "../../utils/serializeState";
 
-const saveProgress = (reduxState) => { 
-    const serializedState = serializeState(reduxState)
-    fetch('/users/save', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({state: serializedState})
-    })
-    .then(response => {
+const saveProgress = (reduxState) => {
+  const serializedState = serializeState(reduxState);
+  fetch("/users/save", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ state: serializedState }),
+  })
+    .then((response) => {
       if (response.ok) {
-        console.log('Progress saved')
+        console.log("Progress saved");
       } else {
-        console.log('Failed to save progress')
+        console.log("Failed to save progress");
       }
     })
-    .catch(error => console.log('Failed to save progress', error))
+    .catch((error) => console.log("Failed to save progress", error));
 };
 
-export default saveProgress
+export default saveProgress;
