7f99f9ec8325136018ad435a48f46014d098781b
eeroverlord <112696364+eeroverlord@users.noreply.github.com>
2024-03-26 11:57:54 +0200

    server.js -> const fs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
